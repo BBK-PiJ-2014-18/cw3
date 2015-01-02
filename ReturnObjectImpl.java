2279e661078ee1d07122bd1a9a9d4c6f5c66029b
Mark Kingsbury <lking18@dcs.bbk.ac.uk>
2015-01-02 15:06:15 +0000

    first passing objects

--- ReturnObjectImpl.java
+++ ReturnObjectImpl.java
@@ -1,20 +1,35 @@
 
 public class ReturnObjectImpl implements ReturnObject {
 
-
+	private Object content;
+	
+	//constructor
+	public ReturnObjectImpl () {
+		this.content = null;
+	}
+	
+	
+	//setter
+	public ReturnObjectImpl (Object item) {
+		this.content = item;
+	}	
+	
+	
+	
 	public boolean hasError() {
 	
-	
+		return true;
 	}
 
 	public ErrorMessage getError() {
-	
+		
+		return ErrorMessage.NO_ERROR;	
 	
 	}
 
 	public Object getReturnValue() {
 	
-		
+		return this.content;
 	
 	}
 
