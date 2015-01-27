a2870419362fb2a3cbf60972f7176ad702efc2ab
Mark Kingsbury <lking18@dcs.bbk.ac.uk>
2015-01-27 15:11:41 +0000

    good LinkedList & ReturnObjectImpl

--- LinkedList.java
+++ LinkedList.java
@@ -1,15 +1,15 @@
 public class LinkedList implements List {
 
 	private Object item;
-	private LinkedList nextItem;
+	private LinkedList nextNode;
 
 	public LinkedList() {
 		this.item = null;
-		this.nextItem = null; 
+		this.nextNode = null; 
 	}	
 
 	public boolean isEmpty() {
-		if(this.nextItem == null) {
+		if(this.nextNode == null) {
 			return true;
 		} else {
 			return false;
@@ -20,10 +20,10 @@ public class LinkedList implements List {
 		if(this.isEmpty()) {
 			return 0;
 		} else {
-		LinkedList currentItem = this.nextItem;
-		int result = 1;
-		while(currentItem.nextItem != null) {
-			currentItem = currentItem.nextItem;
+		LinkedList currentNode = this;
+		int result = 0;
+		while(currentNode.nextNode != null) {
+			currentNode = currentNode.nextNode;
 			result ++;	
 		}			
 		return result;
@@ -31,45 +31,87 @@ public class LinkedList implements List {
 	}
 
 	public ReturnObject get(int index) {	
-		int currentNode = 0;
-		LinkedList currentItem = this.nextItem;
-		while (currentNode < index) {
-			currentItem = currentItem.nextItem;
-			currentNode++;
+		if(checkIndex(index).hasError()) {
+			return checkIndex(index);
 		}
-		ReturnObject result = new ReturnObjectImpl(currentItem.item);
-		return result;
+		LinkedList currentNode = getNodeBeforeIndex(index);		
+		return new ReturnObjectImpl(currentNode.nextNode.item);	
 	}
-
+	
 	public ReturnObject remove(int index) {
-		int currentNode = 0;
-		LinkedList currentItem = this;
-		while (currentNode < index) {
-			currentItem = currentItem.nextItem;
-			currentNode++;
-		}
-		ReturnObject result = new ReturnObjectImpl(currentItem.nextItem.item);	
-		currentItem.nextItem = currentItem.nextItem.nextItem;
+		if(checkIndex(index).hasError()) {
+			return checkIndex(index);
+		}	
+		LinkedList currentNode = getNodeBeforeIndex(index);		
+		ReturnObject result = new ReturnObjectImpl(currentNode.nextNode.item);	
+		currentNode.nextNode = currentNode.nextNode.nextNode;
 		return result;
 	}
 
-//	public ReturnObject add(int index, Object item) {
-//	}
-
-
+	public ReturnObject add(int index, Object item) {
+		if(checkIndex(index).hasError()) {
+			return checkIndex(index);
+		}
+		if(checkItem(item).hasError()) {
+			return checkItem(item);
+		}
+		LinkedList currentNode = getNodeBeforeIndex(index);	
+		LinkedList newNode = new LinkedList();
+		newNode.item = item;
+		newNode.nextNode = currentNode.nextNode;
+		currentNode.nextNode = newNode;
+		return new ReturnObjectImpl(ErrorMessage.NO_ERROR); 	
+	}
+		
 	public ReturnObject add(Object item) {
-		ReturnObject result = new ReturnObjectImpl(item);
+		if(checkItem(item).hasError()) {
+			return checkItem(item);
+		}
+		ReturnObject result = new ReturnObjectImpl(ErrorMessage.NO_ERROR);
 		LinkedList newNode = new LinkedList();
 		newNode.item = item;
 		if(this.isEmpty()) {
-			this.nextItem = newNode;
+			this.nextNode = newNode;
 			return result;
-		}	
-		LinkedList currentItem = this.nextItem;
-		while(currentItem.nextItem != null) {
-			currentItem = currentItem.nextItem;	
+		}
+		LinkedList currentNode = this.nextNode;
+		while(currentNode.nextNode != null) {
+			currentNode = currentNode.nextNode;	
 		}			
-		currentItem.nextItem = newNode;
+		currentNode.nextNode = newNode;
 		return result;
+	}
+	
+	// TODO JavaDoc
+	
+	private LinkedList getNodeBeforeIndex(int index) {
+		int countNodes = 0;
+		LinkedList currentNode = this;
+		while (countNodes < index) {
+			currentNode = currentNode.nextNode;
+			countNodes++;
+		}
+		return currentNode;
+	}
+
+	// TODO JavaDoc
+
+	private ReturnObject checkIndex(int index) {
+		if(isEmpty()) {
+			return new ReturnObjectImpl(ErrorMessage.EMPTY_STRUCTURE);
+		}	
+		if(index < 0 || index >= size()) {
+			return new ReturnObjectImpl(ErrorMessage.INDEX_OUT_OF_BOUNDS);
+		}
+		return new ReturnObjectImpl(ErrorMessage.NO_ERROR);
+	}
+
+	// TODO JavaDoc
+	
+	private ReturnObject checkItem(Object item) {
+		if(item == null) {
+			return new ReturnObjectImpl(ErrorMessage.INVALID_ARGUMENT);
+		}
+		return new ReturnObjectImpl(ErrorMessage.NO_ERROR);
 	}
 }
--- ReturnObjectImpl.java
+++ ReturnObjectImpl.java
@@ -2,26 +2,38 @@
 public class ReturnObjectImpl implements ReturnObject {
 
 	private Object content;
+	private ErrorMessage error;
 	
-	//constructor
+	//constructor - delete this constructor
 	public ReturnObjectImpl () {
 		this.content = null;
+		this.error = null;
 	}
 
 	public ReturnObjectImpl (Object content) {
 		this.content = content;
-	}	
+		this.error = ErrorMessage.NO_ERROR;
+	}
 	
+	public ReturnObjectImpl (ErrorMessage error) {
+		this.content = null;
+		this.error = error;
+	}	
+		
 	public boolean hasError() {
-		return true;
+		if(error == ErrorMessage.NO_ERROR) {
+			return false;
+		} else {
+			return true;
+		}		
 	}
 
 	public ErrorMessage getError() {
-		return ErrorMessage.NO_ERROR;	
+		return error;	
 	}
 
 	public Object getReturnValue() {		
-		return content;
+		return content;	
 	}
 
 }
