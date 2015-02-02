c06f596ebf55a0b2f34fad8006e1bfe02dff8d2b
Mark Kingsbury <lking18@dcs.bbk.ac.uk>
2015-02-02 13:08:52 +0000

    done other than JavaDoc

--- FunctionalArrayList.java
+++ FunctionalArrayList.java
@@ -7,7 +7,7 @@ public class FunctionalArrayList extends ArrayList implements FunctionalList {
 	}
 
 	@Override
-    public FunctionalList rest() {
+	public FunctionalList rest() {
     	FunctionalList result = new FunctionalArrayList();
 		for (int i = 1; i < size(); i++) {
 			result.add(this.get(i).getReturnValue());
